e24ffb979d59929004a59a715ae471897f759125
Zach Garrison <zgedina@yahoo.com>
2023-06-24 14:12:04 -0500

    got it rendering tasks

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     console.log("JQ")
-
-    
+    getTasks()
 });
 
 
@@ -10,7 +9,7 @@ function getTasks(){
     type: "GET",
     url: "/tasks",
   }) .then((response) => {
-    renderTasks(response);
+    renderTasks(response)
   })
   .catch((error) => {
     console.log("error in get client", error);
@@ -19,12 +18,11 @@ function getTasks(){
 
 
 function renderTasks(tasks) {
-    $("#taskBody").empty();
-  
+    $("#task-body").empty();
     for (let i = 0; i < tasks.length; i += 1) {
       let task = tasks[i];
       let newRow = $(`
-      <tr class="table-danger" >
+      <tr class="table-danger">
                         <th scope="row" data-id = ${task.id}>${task.id}</th>
                         <td>${task.task}</td>
                         <td>${task.completed}</td>
@@ -32,14 +30,11 @@ function renderTasks(tasks) {
                           <button type="submit" class="btn btn-danger">Delete</button>
                           <button type="submit" class="btn btn-success ms-1">Finish</button>
                         </td>
-                        <td>${task.date}</td>
+                        <td>${task.date.split('T')[0]}</td>
                       </tr>
-      <td><button class = "transfer-button">Ready to transfer!</button></td>
-      <td><button class = "delete-button">Delete</button></td>
       </tr>
-  
       `);
   
-      $("#taskBody").append(newRow);
+      $("#task-body").append(newRow);
     }
   } // end renderTasks
