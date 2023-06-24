7f5fbdf845e7a833dcc730065417f7f60f705523
Zach Garrison <zgedina@yahoo.com>
2023-06-24 16:02:28 -0500

    updated database sql

--- database.sql
+++ database.sql
@@ -1,8 +1,10 @@
 CREATE TABLE "tasks" (
 	"id" serial primary key,
 	"task" varchar(255),
-	"completed" boolean default false,
-	"date" DATE
+	"completed" boolean default false
 	);
+	
+INSERT INTO "tasks" ("task", "completed")
+VALUES ('Go on a walk', false);
 
-
+-- DROP TABLE "tasks"
